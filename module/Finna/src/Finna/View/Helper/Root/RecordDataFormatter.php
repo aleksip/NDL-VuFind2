374c2eb13fa3bfb70320ed77ef0906186b62c760
Aleksi Peebles <aleksi.peebles@helsinki.fi>
2020-02-21 14:55:14 +0200

    Improve RecordDataFormatter::getGroupedFields documentation

--- module/Finna/src/Finna/View/Helper/Root/RecordDataFormatter.php
+++ module/Finna/src/Finna/View/Helper/Root/RecordDataFormatter.php
@@ -223,6 +223,9 @@ class RecordDataFormatter extends \VuFind\View\Helper\Root\RecordDataFormatter
     /**
      * Get a spec of field groups.
      *
+     * The $lines array is passed as a reference and may be modified depending
+     * on the value of $unused (see FieldGroupBuilder::UNUSED_REMOVE_USED).
+     *
      * @param array  $groups        Array specifying the groups.
      * @param array  $lines         All lines used in the groups.
      * @param string $template      Default group template to use if not
