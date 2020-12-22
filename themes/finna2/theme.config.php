d9d1ddd9000a9958242ef73e2db04f249dedf17f
Aleksi Peebles <aleksi.peebles@helsinki.fi>
2020-12-22 13:29:11 +0200

    Remove AttributesHelper

--- themes/finna2/theme.config.php
+++ themes/finna2/theme.config.php
@@ -3,7 +3,6 @@ $config = [
     'extends' => 'bootstrap3',
     'helpers' => [
         'factories' => [
-            'Finna\View\Helper\Root\AttributesHelper' => 'Laminas\ServiceManager\Factory\InvokableFactory',
             'Finna\View\Helper\Root\Auth' => 'Finna\View\Helper\Root\AuthFactory',
             'Finna\View\Helper\Root\AuthorizationNotification' => 'Finna\View\Helper\Root\AuthorizationNotificationFactory',
             'Finna\View\Helper\Root\Authority' => 'Finna\View\Helper\Root\AuthorityFactory',
@@ -69,7 +68,6 @@ $config = [
             'Finna\View\Helper\Root\UserPublicName' => 'Laminas\ServiceManager\Factory\InvokableFactory',
         ],
         'aliases' => [
-            'attributes' => 'Finna\View\Helper\Root\AttributesHelper',
             'auth' => 'Finna\View\Helper\Root\Auth',
             'authority' => 'Finna\View\Helper\Root\Authority',
             'authorizationNote' => 'Finna\View\Helper\Root\AuthorizationNotification',
