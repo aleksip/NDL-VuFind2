5f74f9c93325eff32eb5e1d43f8b2e1c18f25ce5
Aleksi Peebles <aleksi.peebles@helsinki.fi>
2021-01-15 16:25:48 +0200

    A function needs a name

--- themes/finna2/js/finna-recommendation-memory.js
+++ themes/finna2/js/finna-recommendation-memory.js
@@ -1,7 +1,7 @@
 /* global finna */
 finna.recommendationMemory = (function finnaRecommendationMemory() {
   function b64EncodeUnicode(str) {
-    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function replacer(match, p1) {
       return String.fromCharCode(parseInt(p1, 16))
     }))
   }
