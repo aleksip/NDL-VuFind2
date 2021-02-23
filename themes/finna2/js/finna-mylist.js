4f027cb12dc7d27648ce59d85c589a3137ccb744
Aleksi Peebles <aleksi.peebles@helsinki.fi>
2021-02-23 17:45:07 +0200

    Initial refactoring into component files

--- themes/finna2/js/finna-mylist.js
+++ themes/finna2/js/finna-mylist.js
@@ -6,94 +6,6 @@ finna.myList = (function finnaMyList() {
   var save = false;
   var listUrl = null;
   var refreshLists = null;
-  var truncateDone = '<div class="truncate-field" data-rows="1" data-row-height="5" markdown="1"';
-  var truncateTag = '<truncate>';
-  var truncateCloseTag = '</truncate>';
-  function getEditorCursorPos(mdeditor) {
-    var doc = mdeditor.codemirror.getDoc();
-    var cursorPos = doc.getCursor();
-    var position = {
-      line: cursorPos.line,
-      ch: cursorPos.ch
-    };
-    return position;
-  }
-
-  function insertElement(element, mdeditor) {
-    var doc = mdeditor.codemirror.getDoc();
-    doc.replaceRange(element, getEditorCursorPos(mdeditor));
-    mdeditor.codemirror.focus();
-  }
-
-  function toggleTruncateField(mdeditor) {
-    var value = mdeditor.value();
-    if (value.indexOf(truncateTag) !== -1) {
-      return;
-    } else {
-      var truncateEl = '\n' + truncateTag + '<summary></summary>\n\n' + truncateCloseTag;
-      insertElement(truncateEl, mdeditor);
-      var doc = editor.codemirror.getDoc();
-      var cursorPos = getEditorCursorPos(editor);
-      doc.setCursor({line: cursorPos.line - 2, ch: '<truncate><summary>'.length});
-    }
-  }
-
-  function insertDetails(mdeditor) {
-    var summaryPlaceholder = VuFind.translate('details_summary_placeholder');
-    var detailsElement = '\n<details class="favorite-list-details" markdown="1">' +
-     '<summary markdown="1">' + summaryPlaceholder + '</summary>\n' +
-     VuFind.translate('details_text_placeholder') + '\n' +
-     '</details>';
-
-    insertElement(detailsElement, mdeditor);
-    var doc = editor.codemirror.getDoc();
-    var cursorPos = getEditorCursorPos(editor);
-    var summaryAndPlaceholder = '<summary>' + summaryPlaceholder;
-    doc.setCursor({line: cursorPos.line - 1, ch: summaryAndPlaceholder.length});
-  }
-
-  var mdeToolbar = [
-    'bold', 'italic',
-    'heading', '|',
-    'quote', 'unordered-list',
-    'ordered-list', '|',
-    'link', 'image',
-    '|',
-    {
-      name: 'Details',
-      action: function detailsInsert(mdeditor) {
-        insertDetails(mdeditor);
-      },
-      className: 'fa details-icon',
-      title: 'Insert details element'
-    },
-    {
-      name: 'truncate',
-      action: function truncateFieldToggle(mdeditor) {
-        toggleTruncateField(mdeditor);
-      },
-      className: 'fa fa-pagebreak',
-      title: 'Truncate'
-    },
-    {
-      name: 'close',
-      action: function closeToolbar() {
-        $(document).trigger('click');
-      },
-      className: 'fa fa-times editor-toolbar-close',
-      title: 'Close'
-    }
-  ];
-
-  function initDetailsElements() {
-    $('.favorite-list-details').click(function onDetailsClick() {
-      if ($(this).attr('open') === 'open') {
-        $(this).attr('open', false);
-      } else {
-        $(this).attr('open', 'open');
-      }
-    });
-  }
 
   // This is duplicated in image-popup.js to avoid dependency
   function getActiveListId() {
@@ -126,50 +38,6 @@ finna.myList = (function finnaMyList() {
     target.toggleClass('fa-spinner fa-spin list-save', mode);
   }
 
-  function handleTruncateField(description, addTruncate) {
-    var trunc = typeof addTruncate !== 'undefined' ? addTruncate : true;
-    var desc = description;
-    var summaryText = '';
-    var truncateEl = '';
-    var tempDom = '';
-    if (trunc && description.indexOf(truncateTag) !== -1) {
-      // Fixes preview bug
-      desc = desc.replace('<p><truncate>', '<truncate>');
-
-      tempDom = $('<div>').append($.parseHTML(desc));
-      // Replace <truncate> with <div class="truncate-field"..>
-      truncateEl = $(tempDom).find('truncate');
-      truncateEl.wrap(truncateDone + '>');
-      var newTruncate = tempDom.find('.truncate-field');
-      truncateEl.contents().unwrap();
-      newTruncate.find('details').wrap('<div>');
-
-      // Remove <summary> element and add its value to data-label attribute
-      if (newTruncate.find(':first-child').is('summary')) {
-        summaryText = newTruncate.find(':first-child')[0];
-        newTruncate.find(':first-child')[0].remove();
-      }
-      if (typeof summaryText.innerHTML !== 'undefined') {
-        newTruncate.attr('data-label', summaryText.innerHTML);
-      }
-      desc = tempDom[0].innerHTML;
-    } else if (desc.indexOf(truncateDone) !== -1) {
-      tempDom = $('<div>').append($.parseHTML(desc));
-      // Replace <div class="truncate-field"..> with <truncate> tag and create summary element
-      truncateEl = $(tempDom).find('.truncate-field');
-      summaryText = truncateEl.attr('data-label');
-      if (typeof summaryText === 'undefined') {
-        summaryText = '';
-      }
-      truncateEl.prepend($('<summary>' + summaryText + '</summary>'));
-      truncateEl.wrap('<truncate>');
-      tempDom.find('.truncate-field details').unwrap();
-      tempDom.find('.truncate-field').children().unwrap();
-      desc = tempDom[0].innerHTML;
-    }
-    return desc;
-  }
-
   function updateList(params, callback, type) {
     save = true;
     var spinner = null;
@@ -182,7 +50,7 @@ finna.myList = (function finnaMyList() {
 
     if (type !== 'add-list') {
       var description = $('.list-description .editable [data-markdown]').data('markdown');
-      description = handleTruncateField(description);
+      description = finna.mdEditor.handleTruncateField(description);
       if (description === VuFind.translate('add_list_description')) {
         listParams.desc = '';
       } else {
@@ -441,84 +309,13 @@ finna.myList = (function finnaMyList() {
 
       toggleTitleEditable(false);
 
-      element.toggleClass('edit', true);
-      var container = element.find('[data-markdown]');
-
-      var textArea = $('<textarea/>');
-      var currentVal = null;
-      currentVal = container.data('markdown');
-      currentVal = handleTruncateField(currentVal, false);
-      textArea.text(currentVal);
-      container.hide();
-      textArea.insertAfter(container);
-      if (editor) {
-        editor = null;
-      }
-
-      var editorSettings = {
-        autoDownloadFontAwesome: false,
-        autofocus: true,
-        element: textArea[0],
-        toolbar: mdeToolbar,
-        spellChecker: false,
-        status: false
-      };
-
-      editor = new EasyMDE(editorSettings);
-      currentVal = editor.value();
-
-      if (currentVal.indexOf(truncateTag) !== -1) {
-        $('.fa-pagebreak').addClass('pagebreak-toggled');
-      }
-      // Preview
-      var html = editor.options.previewRender(editor.value());
-      html = handleTruncateField(html);
-      $('.markdown-preview').remove();
-      var preview = $('<div/>').addClass('markdown-preview')
-        .html($('<div/>').addClass('data').html(html));
-      $('<div/>').addClass('preview').text(VuFind.translate('preview').toUpperCase()).prependTo(preview);
-      preview.appendTo(element);
-      finna.layout.initTruncate(preview);
-      initDetailsElements();
-
-      editor.codemirror.on('change', function onChangeEditor() {
-        var result = editor.options.previewRender(editor.value());
-        if (result.indexOf(truncateTag) !== -1) {
-          if (!$('.fa-pagebreak').hasClass('pagebreak-toggled')) {
-            $('.fa-pagebreak').addClass('pagebreak-toggled');
-          }
-        } else {
-          $('.fa-pagebreak').removeClass('pagebreak-toggled');
-        }
-        result = handleTruncateField(result);
-        preview.find('.data').html(result);
-        finna.layout.initTruncate(preview);
-        initDetailsElements();
-      });
+      editor = finna.mdEditor.initEditableMarkdownField(element, callback);
 
       // Close editor and save when user clicks outside the editor
       $(document).one('click', function onClickDocument() {
-        var markdown = editor.value();
-        var resultHtml = editor.options.previewRender(markdown)
-
-        editor.toTextArea();
+        var markdown = finna.mdEditor.closeEditor();
         editor = null;
-        element.toggleClass('edit', false).find('textarea').remove();
-
-        container.show();
-        container.data('markdown', markdown);
-        container.data('empty', markdown.length === 0 ? '1' : '0');
-        resultHtml = handleTruncateField(resultHtml);
-        container.html(resultHtml);
-        finna.layout.initTruncate(container);
-        preview.remove();
-
         callback(markdown);
-      });
-      $('.CodeMirror-code').focus();
-      // Prevent clicks within the editor area from bubbling up and closing the editor.
-      element.closest('.markdown').unbind('click').click(function onClickEditor() {
-        return false;
       });
     });
   }
