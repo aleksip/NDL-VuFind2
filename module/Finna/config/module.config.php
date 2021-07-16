944beb2ecf0bda293c83895d7949dfcaa85aa833
Aleksi Peebles <aleksi.peebles@helsinki.fi>
2021-07-16 14:34:17 +0300

    Add finna-tabs component and custom element

--- module/Finna/config/module.config.php
+++ module/Finna/config/module.config.php
@@ -910,11 +910,13 @@ $config = [
                 'factories' => [
                     'Finna\View\CustomElement\FinnaList' => 'Finna\View\CustomElement\AbstractBaseFactory',
                     'Finna\View\CustomElement\FinnaPanel' => 'Finna\View\CustomElement\AbstractBaseFactory',
+                    'Finna\View\CustomElement\FinnaTabs' => 'Finna\View\CustomElement\AbstractBaseFactory',
                     'Finna\View\CustomElement\FinnaTruncate' => 'Finna\View\CustomElement\AbstractBaseFactory',
                 ],
                 'aliases' => [
                     'finna-list' => 'Finna\View\CustomElement\FinnaList',
                     'finna-panel' => 'Finna\View\CustomElement\FinnaPanel',
+                    'finna-tabs' => 'Finna\View\CustomElement\FinnaTabs',
                     'finna-truncate' => 'Finna\View\CustomElement\FinnaTruncate',
                 ]
             ]
